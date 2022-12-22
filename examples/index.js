767fefe59b390972fd5150d8b25148ec3598f856
Thien Tran <tranct@sea.com>
2022-12-22 12:34:20 +0800

    feat: test fail in the relative require of source code

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,9 @@
-function add(number1, number2) {
-  return number1 + number2;
-}
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+//
+
+const { add } = require('./src/add.js');
 
 function multiply(number1, number2) {
   return number1 * number2;
