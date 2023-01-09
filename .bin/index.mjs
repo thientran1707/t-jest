e7688dee6fddda1d89a538f79c6e6f21e553a251
Thien Tran <tranct@sea.com>
2023-01-09 18:18:24 +0800

    feat: add yargs and read config from cmd line

--- .bin/index.mjs
+++ .bin/index.mjs
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-import runTest from '../src/index.mjs';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
+import path from 'path';
+import runTests from '../src/index.mjs';
 
-runTest();
+const { config } = yargs(hideBin(process.argv)).argv;
+const configPath = path.join(process.cwd(), config);
+
+runTests({ configPath });
