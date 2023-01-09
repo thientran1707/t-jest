af11ffbace05d1ff3dd2b83f8610f7779d52bed3
Thien Tran <tranct@sea.com>
2023-01-09 17:44:08 +0800

    feat: basic structure

--- .bin/index.mjs
+++ .bin/index.mjs
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import yargs from 'yargs/yargs';
-import { hideBin } from 'yargs/helpers';
-import path from 'path';
 import runTest from '../src/index.mjs';
 
-const { config } = yargs(hideBin(process.argv)).argv;
-const configPath = path.join(process.cwd(), config);
-
-runTest({ configPath });
+runTest();
--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,3 @@
-// Utils
-import { searchTestFiles } from './search/index.mjs';
-
-// Native modules
-import path from 'path';
-import fs from 'fs';
-
-export default async function runTest({
-  configPath
-}) {
-  const { dir: rootDir, base: configFileName } = path.parse(configPath);
-
-  const {
-    testMatch,
-    testPathIgnorePatterns,
-  } = await import(configPath).then(module => module.default);
-
-  const testFiles = searchTestFiles({ testMatch, testPathIgnorePatterns });
-
-  console.log('testFiles = ', testFiles);
+export default function runTests() {
+  console.log('runTests');
 }
