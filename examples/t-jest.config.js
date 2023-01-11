0882c1618f0d486ef6e6220206a7e62a71ce3267
Thien Tran <tranct@sea.com>
2023-01-11 13:58:08 +0800

    feat: add test examples and config

--- examples/t-jest.config.js
+++ examples/t-jest.config.js
@@ -1,1 +1,4 @@
-module.exports = {}
+module.exports = {
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", '**/exclude.test.js'],
+};
