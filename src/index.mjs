1188b9209bc8dcc44aba6ab89e1c844d1c0ef931
Thien Tran <tranct@sea.com>
2023-01-11 14:07:04 +0800

    feat: add search files with glob-all

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,17 @@
+// Utils
+import { searchTestFiles } from './search/index.mjs';
+
 // Native module
 import path from 'path';
 
 export default async function runTests({ configPath }) {
   const { dir: rootDir, base: configFileName } = path.parse(configPath);;
 
-  const config = await import(configPath).then(module => module.default);
-  console.log('config = ', config);
+  const {
+    testMatch,
+    testPathIgnorePatterns,
+  } = await import(configPath).then(module => module.default);
+
+  const testFiles = searchTestFiles({ testMatch, testPathIgnorePatterns });
+  console.log('testFiles = ', testFiles);
 }
---
+++ src/search/constants.mjs
@@ -0,0 +1,4 @@
+
+// Default value get inspired by Jest config
+export const DEFAULT_TEST_MATCH = ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ];
+export const DEFAULT_TEST_PATH_IGNORE_PATTERNS = ["/node_modules/"];
