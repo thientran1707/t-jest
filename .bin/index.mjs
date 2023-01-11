5cef3396e24cf05eecd851ce51c7b222309c9937
Thien Tran <tranct@sea.com>
2023-01-11 13:09:13 +0800

    feat: read test config content

--- .bin/index.mjs
+++ .bin/index.mjs
@@ -2,9 +2,9 @@
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 import path from 'path';
-import runTest from '../src/index.mjs';
+import runTests from '../src/index.mjs';
 
 const { config } = yargs(hideBin(process.argv)).argv;
 const configPath = path.join(process.cwd(), config);
 
-runTest({ configPath });
+runTests({ configPath });
--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,9 @@
-export default function runTests({ configPath }) {
-  console.log('runTests, configPath = ', configPath);
+// Native module
+import path from 'path';
+
+export default async function runTests({ configPath }) {
+  const { dir: rootDir, base: configFileName } = path.parse(configPath);;
+
+  const config = await import(configPath).then(module => module.default);
+  console.log('config = ', config);
 }
