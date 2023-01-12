a4bc0a5018f52afe1c0b36fed20f9bdc187cdad2
Thien Tran <tranct@sea.com>
2023-01-12 23:48:33 +0800

    feat: prettify expect error message

--- src/assertion/index.mjs
+++ src/assertion/index.mjs
@@ -1,8 +1,10 @@
+import chalk from 'chalk';
+
 export const expect = received => {
   return {
     toBe: expected => {
       if (received !== expected) {
-        throw new Error(`Expected ${expected} but received ${received}`);
+        throw new Error(`Expected ${chalk.green(expected)} but received ${chalk.red(received)}`);
       }
 
       return true;
