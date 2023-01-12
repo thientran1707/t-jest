723122aa2d2bc422a9b8d0978a0ff2d37a3a22a6
Thien Tran <tranct@sea.com>
2023-01-12 23:25:51 +0800

    feat: add async.test.js

---
+++ examples/__test__/async.test.js
@@ -0,0 +1,8 @@
+describe('simple async test with describe and it', () => {
+  it('should works', async () => {
+    const result = await new Promise(resolve => setTimeout(() => {
+      resolve(1);
+    }, 200));
+    expect(result).toBe(1);
+  })
+});
